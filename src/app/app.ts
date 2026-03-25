67b6b99956d7cf3612a55f305dbc97509ece4570
LanaNew <l4n.steen@gmail.com>
2026-03-25 21:56:11 +0400

    second commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
+import { Home } from './home/home';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [Footer, Header, Home]
 })
 export class App {
-  protected readonly title = signal('ang1-8');
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
